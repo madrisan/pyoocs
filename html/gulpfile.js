48767e564885280d0adc200fe82b1a73a5c26615
Davide Madrisan <davide.madrisan@gmail.com>
2016-01-02 17:12:29 +0100

    html (gulp): create a variable containg the destination folder
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -16,6 +16,10 @@ var gulp        = require('gulp'),
     del         = require('del'),
     less        = require('gulp-less');
 
+var paths = {
+    dist: 'json-server/public/'
+};
+
 gulp.task('jshint', function() {
     return gulp.src('app/scripts/**/*.js')
             .pipe(jshint())
@@ -24,7 +28,7 @@ gulp.task('jshint', function() {
 
 // Clean
 gulp.task('clean', function() {
-    return del(['json-server/public']);
+    return del([paths.dist]);
 });
 
 // Default task
@@ -38,34 +42,34 @@ gulp.task('usemin', ['buildless', 'jshint'], function () {
                 css:[minifycss(), rev()],
                 js: [ngannotate(), uglify(), rev()]
             }))
-            .pipe(gulp.dest('json-server/public/'));
+            .pipe(gulp.dest(paths.dist));
 });
 
 // Views
 gulp.task('copyviews', function() {
     return gulp.src('app/views/**/*.html')
-            .pipe(gulp.dest('json-server/public/views/'));
+            .pipe(gulp.dest(paths.dist + '/views/'));
 });
 
 // Images
 gulp.task('imagemin', function() {
-    return del(['json-server/public/images']), gulp.src('app/images/**/*')
+    return del([paths.dist + '/images/']), gulp.src('app/images/**/*')
             .pipe(cache(imagemin({
                 optimizationLevel: 3,
                 progressive: true,
                 interlaced: true
             })))
-            .pipe(gulp.dest('json-server/public/images'))
+            .pipe(gulp.dest(paths.dist + '/images/'))
             .pipe(notify({ message: 'Images task complete' }));
 });
 
 // Fonts
 gulp.task('copyfonts', function() {
     gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-     .pipe(gulp.dest('json-server/public/fonts'));
+     .pipe(gulp.dest(paths.dist + '/fonts/'));
 
     gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
-     .pipe(gulp.dest('json-server/public/fonts'));
+     .pipe(gulp.dest(paths.dist + '/fonts/'));
 });
 
 // CSS
@@ -95,16 +99,16 @@ gulp.task('browser-sync', ['default'], function () {
         'app/styles/**/*.css',
         'app/images/**/*.png',
         'app/scripts/**/*.js',
-        'json-server/public/**/*'
+        paths.dist + '/**/*'
     ];
 
     browserSync.init(files, {
         server: {
-            baseDir: "json-server/public",
+            baseDir: paths.dist,
             index: "index.html"
         }
     });
 
     // Watch any files in dist/, reload on change
-    gulp.watch(['json-server/public/**']).on('change', browserSync.reload);
+    gulp.watch([paths.dist + '/**']).on('change', browserSync.reload);
 });
