d0f31ff6b322225680be13af7a8fe1538151c587
madrisan <davide.madrisan@gmail.com>
2016-01-26 20:33:54 +0200

    html: simplify the js code
    Signed-off-by: madrisan <davide.madrisan@gmail.com>

--- html/app/scripts/controllers.js
+++ html/app/scripts/controllers.js
@@ -12,7 +12,9 @@ var getkeys = function(data) {
 
 oocsAppModule.controller('ScanController', ['$scope', 'ScanService',
     function($scope, ScanService) {
+
         'use strict';
+
         $scope.showServerList = false;
         $scope.urls = [];
 
@@ -29,80 +31,79 @@ oocsAppModule.controller('ScanController', ['$scope', 'ScanService',
     }
 ]);
 
-oocsAppModule.controller('ScanDetailController', ['$scope', '$stateParams', 'ScanDetailService',
+oocsAppModule.controller('ScanDetailController',
+    ['$scope', '$stateParams', 'ScanDetailService',
     function($scope, $stateParams, ScanDetailService) {
+
         'use strict';
-        var dispatchJSONdata = function() {
-            //console.log('executing the dispatchJSONdata() body...');
 
-            $scope.showScan = false;
-            $scope.message = "Loading ...";
+        //console.log('executing the dispatchJSONdata() body...');
 
-            $scope.hostname = {};
-            $scope.distribution = {};
-            $scope.modules = {};
+        $scope.showScan = false;
+        $scope.message = "Loading ...";
 
-            ScanDetailService.getJSONdata()
-                .get({id:parseInt($stateParams.id, 10)})
-                .$promise.then(
-                    function(response) {
-                        var jsondata = response;
+        $scope.hostname = {};
+        $scope.distribution = {};
+        $scope.modules = {};
 
-                        $scope.hostname = getkeys(jsondata)[0];
-                        //console.log('hostname: ' + $scope.hostname);
+        ScanDetailService.getJSONdata()
+            .get({id:parseInt($stateParams.id, 10)})
+            .$promise.then(
+                function(response) {
+                    var jsondata = response;
 
-                        $scope.distribution = jsondata[$scope.hostname].distribution;
-                        //console.log('distribution: ' + $scope.distribution.description);
+                    $scope.hostname = getkeys(jsondata)[0];
+                    //console.log('hostname: ' + $scope.hostname);
 
-                        $scope.modules = getkeys(jsondata[$scope.hostname].modules);
-                        //console.log($scope.modules);
+                    $scope.distribution = jsondata[$scope.hostname].distribution;
+                    //console.log('distribution: ' + $scope.distribution.description);
 
-                        $scope.getModuleData = function (moduleName) {
-                            var checks = jsondata[$scope.hostname].modules[moduleName].checks,
-                                status = jsondata[$scope.hostname].modules[moduleName].status;
+                    $scope.modules = getkeys(jsondata[$scope.hostname].modules);
+                    //console.log($scope.modules);
 
-                            return {
-                                checks : checks,
-                                status : status
-                            };
+                    $scope.getModuleData = function (moduleName) {
+                        var checks = jsondata[$scope.hostname].modules[moduleName].checks,
+                            status = jsondata[$scope.hostname].modules[moduleName].status;
+
+                        return {
+                            checks : checks,
+                            status : status
+                        };
+                    };
+
+                    var scan_summary = jsondata[$scope.hostname].summary;
+                    //console.log('summary: ' + JSON.stringify(scan_summary));
+
+                    $scope.max_severity = scan_summary.max_severity;
+                    //console.log('max_severity: ' + $scope.max_severity);
+
+                    $scope.infos = scan_summary.infos;
+                    //console.log('infos: ' + $scope.infos);
+                    $scope.warnings = scan_summary.warnings;
+                    //console.log('warnings: ' + $scope.warnings);
+                    $scope.criticals = scan_summary.criticals;
+                    //console.log('criticals: ' + $scope.criticals);
+
+                    $scope.totals = $scope.infos + $scope.warnings + $scope.criticals;
+
+                    $scope.max_severity_label = function() {
+                        var severities = {
+                            'success' : 'label-success',
+                            'warning' : 'label-warning',
+                            'critical': 'label-danger'
                         };
 
-                        var scan_summary = jsondata[$scope.hostname].summary;
-                        //console.log('summary: ' + JSON.stringify(scan_summary));
+                        var label = severities[$scope.max_severity];
+                        return label ? label : 'label-default';
+                    };
 
-                        $scope.max_severity = scan_summary.max_severity;
-                        //console.log('max_severity: ' + $scope.max_severity);
-
-                        $scope.infos = scan_summary.infos;
-                        //console.log('infos: ' + $scope.infos);
-                        $scope.warnings = scan_summary.warnings;
-                        //console.log('warnings: ' + $scope.warnings);
-                        $scope.criticals = scan_summary.criticals;
-                        //console.log('criticals: ' + $scope.criticals);
-
-                        $scope.totals = $scope.infos + $scope.warnings + $scope.criticals;
-
-                        $scope.max_severity_label = function() {
-                            var severities = {
-                                'success' : 'label-success',
-                                'warning' : 'label-warning',
-                                'critical': 'label-danger'
-                            };
-
-                            var label = severities[$scope.max_severity];
-                            return label ? label : 'label-default';
-                        };
-
-                        $scope.showScan = true;
-                    },
-                    function(response) {
-                        $scope.message =
-                            "Error: " + response.status + " " + response.statusText;
-                    }
-                );
-        };
-
-        dispatchJSONdata();
+                    $scope.showScan = true;
+                },
+                function(response) {
+                    $scope.message =
+                        "Error: " + response.status + " " + response.statusText;
+                }
+            );
 
         $scope.tab = 1;
         $scope.issueClass = "";
