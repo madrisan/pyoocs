295c2c117ae193a1fdef23c65719ed552f488347
madrisan <davide.madrisan@gmail.com>
2016-02-04 21:18:35 +0200

    Modularize the C code
    Signed-off-by: madrisan <davide.madrisan@gmail.com>

--- ext/_oocs.c
+++ ext/_oocs.c
@@ -16,80 +16,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * The function utmp_get_runlevel() is Copyright 2010 Lennart Poettering
  */
 
 #include <Python.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <errno.h>
-
-#ifndef __USE_GNU
-# define __USE_GNU  1
-#endif
-#include <utmpx.h>
-
 #include "_oocs.h"
-
-int utmp_get_runlevel(int *runlevel, int *previous) {
-        struct utmpx *found, lookup = { .ut_type = RUN_LVL };
-        int r;
-        const char *e;
-
-        assert(runlevel);
-
-        /* If these values are set in the environment this takes
-         * precedence. Presumably, sysvinit does this to work around a
-         * race condition that would otherwise exist where we'd always
-         * go to disk and hence might read runlevel data that might be
-         * very new and does not apply to the current script being
-         * executed. */
-
-        e = getenv("RUNLEVEL");
-        if (e && e[0] > 0) {
-                *runlevel = e[0];
-
-                if (previous) {
-                        /* $PREVLEVEL seems to be an Upstart thing */
-
-                        e = getenv("PREVLEVEL");
-                        if (e && e[0] > 0)
-                                *previous = e[0];
-                        else
-                                *previous = 0;
-                }
-
-                return 0;
-        }
-
-        if (utmpxname(_PATH_UTMPX) < 0)
-                return -errno;
-
-        setutxent();
-
-        found = getutxid(&lookup);
-        if (!found)
-                r = -errno;
-        else {
-                int a, b;
-
-                a = found->ut_pid & 0xFF;
-                b = (found->ut_pid >> 8) & 0xFF;
-
-                *runlevel = a;
-                if (previous)
-                        *previous = b;
-
-                r = 0;
-        }
-
-        endutxent();
-
-        return r;
-}
 
 static PyObject * _oocsext_runlevel(PyObject *self, PyObject *args) {
         int runlevel;
