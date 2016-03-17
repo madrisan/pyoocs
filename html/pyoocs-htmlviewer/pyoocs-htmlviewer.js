1b823f6100025e8c6b2eae189eedba68e69911bf
madrisan <davide.madrisan@gmail.com>
2016-03-17 21:12:21 +0200

    Modularize the Node.JS HTML viewer
    Signed-off-by: madrisan <davide.madrisan@gmail.com>

--- html/pyoocs-htmlviewer/pyoocs-htmlviewer.js
+++ html/pyoocs-htmlviewer/pyoocs-htmlviewer.js
@@ -7,8 +7,7 @@ var express = require('express')
   , app = express()
   , path = require('path')
   , fs = require('fs')
-  , url = require('url')
-  , bodyParser = require('body-parser');
+  , url = require('url');
 
 var argv = require('yargs')
     .usage('Usage: $0 -u <baseurl> -p <publicdir> -d <json-scan-dir>')
@@ -40,8 +39,9 @@ var checkDir = function(dir) {
 checkDir(argv.publicdir);
 checkDir(argv.scandir);
 
-jsonFiles = [];
-scanFiles = fs.readdirSync(argv.scandir);
+var jsonFiles = [];
+var scanFiles = fs.readdirSync(argv.scandir);
+
 for (var i in scanFiles) {
     var scanFile = path.join(argv.scandir, scanFiles[i]);
     try {
@@ -53,78 +53,10 @@ for (var i in scanFiles) {
     }
 }
 
-var scanRouter = express.Router();
-scanRouter.use(bodyParser.json());
-
-scanRouter.route('/')
-    .all(function(req, res, next) {
-        res.writeHead(200, { 'Content-Type': 'text/json' });
-        next();
-    })
-    .get(function(req, res, next) {
-        var jsonHeader = [];
-
-        for (var i in jsonFiles) {
-            try {
-                var jsonFile = path.join(argv.scandir, jsonFiles[i]);
-                var jsonObj = JSON.parse(fs.readFileSync(jsonFile));
-
-                var currHost;
-                for (var key in jsonObj.scan) {
-                    if (jsonObj.scan.hasOwnProperty(key)) {
-                        currHost = key;
-                        break;
-                    }
-                }
-                //console.log('currHost: ' + currHost);
-
-                var currSummary = jsonObj.scan[currHost]['summary'];
-                //console.log('currSummary: ' + JSON.stringify(currSummary));
-
-                if (!currHost || !currSummary) throw new Error();
-
-                jsonHeader.push({
-                    'hostname': currHost,
-                    'urlid': i,
-                    'max_severity': currSummary['max_severity'] || 'unknown'
-                });
-            }
-            catch(err) {
-                console.error('Error parsing ' + jsonFiles[i] + ' ... skip');
-            }
-
-        }
-
-        res.write(JSON.stringify(jsonHeader));
-        //console.log(JSON.stringify(jsonHeader));
-        res.end();
-    });
-
-scanRouter.route('/:dishId')
-    .get(function(req, res, next) {
-        if (req.params.dishId > jsonFiles.length) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('<h1>404 Not Found - Scan ID out of bound</h1>');
-        }
-        else {
-            var jsonFile = path.join(argv.scandir, jsonFiles[req.params.dishId]);
-
-            fs.readFile(jsonFile, function(err, data) {
-                if (err) {
-                    res.writeHead(500, { 'Content-Type': 'text/html' });
-                    res.end('<h1>500 Internal Error - Cannot read JSON file</h1>');
-                }
-                res.writeHead(200, { 'Content-Type': 'text/json' });
-                var jsonObj = JSON.parse(data);
-                res.end(JSON.stringify(jsonObj.scan));
-            });
-        }
-    });
-
-app.use('/scan', scanRouter);
+app.use('/scan', require('./routes/scan')(argv.scandir, jsonFiles));
 app.use(express.static(argv.publicdir));
 
-urlTokens = url.parse(argv.baseurl);
+var urlTokens = url.parse(argv.baseurl);
 var uri = {
     hostname: urlTokens.hostname,
     port: urlTokens.port
