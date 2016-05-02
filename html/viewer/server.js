f2d51ea2a90e1700ba05cf948ef94c4aef5f194a
Davide Madrisan <davide.madrisan@gmail.com>
2016-05-02 22:12:06 +0200

    viewer: add the morgan logger
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- html/viewer/server.js
+++ html/viewer/server.js
@@ -3,12 +3,14 @@
 
 var express = require('express')
   , path = require('path')
-  , favicon = require('serve-favicon');
+  , favicon = require('serve-favicon')
+  , logger = require('morgan');
 
 module.exports = function() {
     var app = express();
 
     app.use(favicon(path.join(__dirname, '../server/public/images', 'favicon.ico')));
+    app.use(logger('dev'));
     app.use('/scan', require('./routes/scan')());
     app.use(express.static(path.join(__dirname, '../server/public')));
 
